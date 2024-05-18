d52c769168b59d66a0c216457e47ac1df47228df
AcePolitician <vicu.ciobu@gmail.com>
2024-05-18 16:26:40 -0400

    Initial commit

---
+++ src/main/java/acepolitician/rocketrycraft/Rocketrycraft.java
@@ -0,0 +1,20 @@
+package acepolitician.rocketrycraft;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Rocketrycraft.MODID)
+public class Rocketrycraft {
+    public static final String MODID = "rocketrycraft";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Rocketrycraft() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
